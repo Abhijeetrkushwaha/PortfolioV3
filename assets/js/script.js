071b284dbccb3ae5fcf5ea05ba44ce55dee5437b
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-12-25 08:03:54 +0530

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,31 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  nav: true,
-  dots: false,
-  items: 1,
-})
-
-let selector = (e) => {
-  return document.querySelector(e)
-}
-
-selector('.side-menu-close').addEventListener('click', () => {
-
-  if(selector('.side-menu-close').classList.contains('closed')){
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    nav: true,
+    dots: false,
+    items: 1,
+  })
+  
+  let selector = (e) => {
+    return document.querySelector(e)
+  }
+  
+  selector('.side-menu-close').addEventListener('click', () => {
+  
+    if(selector('.side-menu-close').classList.contains('closed')){
+      selector('.side-nav-content').classList.remove('opened');
+      selector('.side-menu-close').classList.remove('closed');
+      selector('body').style = 'overflow: visible';
+    } else {
+      selector('.side-nav-content').classList.add('opened');
+      selector('.side-menu-close').classList.add('closed');
+      selector('body').style = 'overflow: hidden';
+    }
+  })
+  
+  $('.nav-link').on('click', () => {
     selector('.side-nav-content').classList.remove('opened');
     selector('.side-menu-close').classList.remove('closed');
     selector('body').style = 'overflow: visible';
-  } else {
-    selector('.side-nav-content').classList.add('opened');
-    selector('.side-menu-close').classList.add('closed');
-    selector('body').style = 'overflow: hidden';
-  }
-})
+  })
+});
