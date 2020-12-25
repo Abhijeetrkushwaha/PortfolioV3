ba1cf142152a8d6ee08c3efadf9c6a484ed59cba
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2020-12-25 07:26:33 +0530

    added project

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,11 @@ selector('.side-menu-close').addEventListener('click', () => {
   if(selector('.side-menu-close').classList.contains('closed')){
     selector('.side-nav-content').classList.remove('opened');
     selector('.side-menu-close').classList.remove('closed');
+    selector('body').style = 'overflow: visible';
   } else {
     selector('.side-nav-content').classList.add('opened');
     selector('.side-menu-close').classList.add('closed');
+    selector('body').style = 'overflow: hidden';
   }
 })
 
